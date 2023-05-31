21cf129026861d6253030c512647918f7aa6d373
danielnugraha <daniel.nata@flower.dev>
2023-05-31 06:57:51 +0100

    make skeleton app

--- FlowerIOS/AppLogic.swift
+++ FlowerIOS/AppLogic.swift
@@ -10,50 +10,6 @@ import CoreML
 import flwr
 
 public class AppLogic: ObservableObject {
-    @Published var hostname: String = ""
-    @Published var port: Int = 8080
-    @Published var taskStatus: TaskStatus = .idle
-    
-    public func startFlowerClient() {
-        taskStatus = .preparing(info: "Preparing flower client")
-        DispatchQueue.global(qos: .default).async {
-            let trainBatchProvider = DataLoader.trainBatchProvider() { count in
-                DispatchQueue.main.async {
-                    self.taskStatus = .preparing(info: "Preparing train dataset: \(count)")
-                }
-            }
-            let testBatchProvider = DataLoader.testBatchProvider() { count in
-                DispatchQueue.main.async {
-                    self.taskStatus = .preparing(info: "Preparing test dataset: \(count)")
-                }
-            }
-            let dataLoader = MLDataLoader(trainBatchProvider: trainBatchProvider, testBatchProvider: testBatchProvider)
-            
-            if let url = Bundle.main.url(forResource: "MNIST_Model", withExtension: "mlmodel") {
-                do {
-                    print(url)
-                    let compiledModelUrl = try MLModel.compileModel(at: url)
-                    print(compiledModelUrl)
-                    let modelInspect = try MLModelInspect(serializedData: Data(contentsOf: url))
-                    let layerWrappers = modelInspect.getLayerWrappers()
-                    let mlFlwrClient = MLFlwrClient(layerWrappers: layerWrappers,
-                                                    dataLoader: dataLoader,
-                                                    compiledModelUrl: compiledModelUrl)
-                    let flwrGRPC = FlwrGRPC(serverHost: self.hostname, serverPort: self.port)
-                    DispatchQueue.main.async {
-                        self.taskStatus = .ongoing(info: "Federated learning started")
-                    }
-                    flwrGRPC.startFlwrGRPC(client: mlFlwrClient) {
-                        DispatchQueue.main.async {
-                            self.taskStatus = .completed(info: "Federated learning completed")
-                        }
-                    }
-                } catch {
-                    print(error)
-                }
-            }
-        }
-    }
 }
 
 public enum TaskStatus: Equatable {
--- FlowerIOS/MainView.swift
+++ FlowerIOS/MainView.swift
@@ -8,43 +8,8 @@
 import SwiftUI
 
 struct MainView: View {
-    @ObservedObject var appLogic = AppLogic()
-    
-    var numberFormatter: NumberFormatter = {
-        var nf = NumberFormatter()
-        nf.usesGroupingSeparator = false
-        nf.numberStyle = .none
-        return nf
-    }()
-    
     var body: some View {
         VStack() {
-            Spacer().frame(height: 50)
-            Text("Flower iOS Client")
-                .font(.largeTitle)
-            Spacer().frame(height: 20)
-            Form {
-                Section(header: Text("Federated Learning")) {
-                    HStack {
-                        Text("Server Hostname: ")
-                        TextField("Server Hostname", text: $appLogic.hostname)
-                            .multilineTextAlignment(.trailing)
-                    }
-                    HStack {
-                        Text("Server Port: ")
-                        TextField( "Server Port", value: $appLogic.port, formatter: numberFormatter)
-                            .multilineTextAlignment(.trailing)
-                    }
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            appLogic.startFlowerClient()
-                        }) {
-                            Text(appLogic.taskStatus.description)
-                        }
-                    }
-                }
-            }
         }
         .background(Color(UIColor.systemGray6))
     }
